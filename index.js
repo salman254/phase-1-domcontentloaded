0ff0985fa5ef714cc80c901f4002fab7a03bcd01
Salman <salmaanmohamed700@gmail.com>
2024-12-07 23:58:00 +0300

    DOM content loaded event

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById("text");
+    
+    paragraph.textContent = "This is really cool!";
+  });
+  
